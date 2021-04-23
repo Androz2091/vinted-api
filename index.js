e64aa4e37db8c9044301f918fff010e97ddb1436
Androz2091 <androz2091@gmail.com>
2021-04-23 12:18:19 +0200

    :bug: Fix memory leak

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const cookie = require('cookie');
  * Fetches a new public cookie from Vinted.fr
  */
 const fetchCookie = async () => {
-    const res = await fetch('https://vinted.fr');
+    const controller = new AbortController();
+    const res = await fetch('https://vinted.fr', {
+        signal: controller.signal
+    });
     const sessionCookie = res.headers.get('set-cookie');
+    controller.abort();
     return cookie.parse(sessionCookie)['secure, _vinted_fr_session'];
 }
 
@@ -25,13 +29,18 @@ const search = async (query, options = {}, cookie) => {
         cookie = await fetchCookie();
     }
 
+    const controller = new AbortController();
     const res = await fetch('https://www.vinted.fr/api/v2/items?' + params, {
+        signal: controller.signal,
         headers: {
             cookie: '_vinted_fr_session=' + cookie
         }
     });
     
     const text = await res.text();
+
+    controller.abort();
+
     try {
         return JSON.parse(text);
     } catch (e) {
