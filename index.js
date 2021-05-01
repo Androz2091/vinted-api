aacbf81e0fdb8f30442e2fe9ea08c2f4de17289c
Androz2091 <androz2091@gmail.com>
2021-05-01 09:46:55 +0200

    :bug: Try to fix memory leaks again

--- index.js
+++ index.js
@@ -5,48 +5,55 @@ const cookie = require('cookie');
 /**
  * Fetches a new public cookie from Vinted.fr
  */
-const fetchCookie = async () => {
-    const controller = new AbortController();
-    const res = await fetch('https://vinted.fr', {
-        signal: controller.signal
+const fetchCookie = () => {
+    return new Promise((resolve, reject) => {
+        const controller = new AbortController();
+        fetch('https://vinted.fr', {
+            signal: controller.signal
+        }).then((res) => {
+            const sessionCookie = res.headers.get('set-cookie');
+            controller.abort();
+            resolve(cookie.parse(sessionCookie)['secure, _vinted_fr_session']);
+        }).catch(() => {
+            controller.abort();
+            reject();
+        });
     });
-    const sessionCookie = res.headers.get('set-cookie');
-    controller.abort();
-    return cookie.parse(sessionCookie)['secure, _vinted_fr_session'];
 }
 
 /**
  * Searches something on Vinted
  */
-const search = async (query, options = {}, cookie) => {
+const search = (query, options = {}) => {
+    return new Promise((resolve, reject) => {
 
-    const params = querystring.encode({
-        search_text: query,
-        ...options
+        const params = querystring.encode({
+            search_text: query,
+            ...options
+        });
+
+        fetchCookie().then((cookie) => {
+            const controller = new AbortController();
+            fetch('https://www.vinted.fr/api/v2/items?' + params, {
+                signal: controller.signal,
+                headers: {
+                    cookie: '_vinted_fr_session=' + cookie
+                }
+            }).then((res) => {
+                res.text().then((text) => {
+                    controller.abort();
+                    try {
+                        resolve(JSON.parse(text));
+                    } catch (e) {
+                        throw text;
+                    }
+                });
+            }).catch(() => {
+                controller.abort();
+                reject('Can not fetch search API');
+            });
+        }).catch(() => reject('Can not fetch cookie'));
     });
-
-    if (!cookie) {
-        cookie = await fetchCookie();
-    }
-
-    const controller = new AbortController();
-    const res = await fetch('https://www.vinted.fr/api/v2/items?' + params, {
-        signal: controller.signal,
-        headers: {
-            cookie: '_vinted_fr_session=' + cookie
-        }
-    });
-    
-    const text = await res.text();
-
-    controller.abort();
-
-    try {
-        return JSON.parse(text);
-    } catch (e) {
-        throw text;
-    }
-
 }
 
 module.exports.fetchCookie = fetchCookie;
