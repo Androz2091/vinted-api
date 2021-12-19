a4ec29fd7c0ae23dda49004bd8b1f885b986438b
Androz <androz2091@gmail.com>
2021-12-19 21:31:22 +0100

    update typings

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,6 @@ declare module 'vinted-api' {
 
     export function fetchCookie (): Promise<string>;
     export function parseVintedQuerystring (url: string): string;
-    export function search (url: string): Promise<VintedSearchResult>;
+    export function search (url: string, disableOrder?: boolean, allowSwap?: boolean, customParams?: Record<string, string|number>): Promise<VintedSearchResult>;
 
 }
