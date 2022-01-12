307210b5f3a32d1d0f4a84acc909794d75a3f856
Androz <androz2091@gmail.com>
2022-01-12 15:21:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,44 +25,50 @@ const fetchCookie = (domain = 'fr') => {
  * Parse a vinted URL to get the querystring usable in the search endpoint
  */
 const parseVintedURL = (url, disableOrder, allowSwap, customParams = {}) => {
-    const decodedURL = decodeURI(url);
-    const matchedParams = decodedURL.match(/^https:\/\/www\.vinted\.([a-z]+)/);
-    if (!matchedParams) return {
-        validURL: false
-    };
+    try {
+        const decodedURL = decodeURI(url);
+        const matchedParams = decodedURL.match(/^https:\/\/www\.vinted\.([a-z]+)/);
+        if (!matchedParams) return {
+            validURL: false
+        };
 
-    const missingIDsParams = ['catalog', 'status'];
-    const params = decodedURL.match(/(?:([a-z_]+)(\[\])?=([a-zA-Z 0-9._À-ú+%]*)&?)/g);
-    if (typeof matchedParams[Symbol.iterator] !== 'function') return {
-        validURL: false
-    };
-    const mappedParams = new Map();
-    for (let param of params) {
-        let [ _, paramName, isArray, paramValue ] = param.match(/(?:([a-z_]+)(\[\])?=([a-zA-Z 0-9._À-ú+%]*)&?)/);
-        if (paramValue?.includes(' ')) paramValue = paramValue.replace(/ /g, '+');
-        if (isArray) {
-            if (missingIDsParams.includes(paramName)) paramName = `${paramName}_id`;
-            if (mappedParams.has(`${paramName}s`)) {
-                mappedParams.set(`${paramName}s`, [ ...mappedParams.get(`${paramName}s`), paramValue ]);
+        const missingIDsParams = ['catalog', 'status'];
+        const params = decodedURL.match(/(?:([a-z_]+)(\[\])?=([a-zA-Z 0-9._À-ú+%]*)&?)/g);
+        if (typeof matchedParams[Symbol.iterator] !== 'function') return {
+            validURL: false
+        };
+        const mappedParams = new Map();
+        for (let param of params) {
+            let [ _, paramName, isArray, paramValue ] = param.match(/(?:([a-z_]+)(\[\])?=([a-zA-Z 0-9._À-ú+%]*)&?)/);
+            if (paramValue?.includes(' ')) paramValue = paramValue.replace(/ /g, '+');
+            if (isArray) {
+                if (missingIDsParams.includes(paramName)) paramName = `${paramName}_id`;
+                if (mappedParams.has(`${paramName}s`)) {
+                    mappedParams.set(`${paramName}s`, [ ...mappedParams.get(`${paramName}s`), paramValue ]);
+                } else {
+                    mappedParams.set(`${paramName}s`, [paramValue]);
+                }
             } else {
-                mappedParams.set(`${paramName}s`, [paramValue]);
+                mappedParams.set(paramName, paramValue);
             }
-        } else {
-            mappedParams.set(paramName, paramValue);
         }
-    }
-    for (let key of Object.keys(customParams)) {
-        mappedParams.set(key, customParams[key]);
-    }
-    const finalParams = [];
-    for (let [ key, value ] of mappedParams.entries()) {
-        finalParams.push(typeof value === 'string' ? `${key}=${value}` : `${key}=${value.join(',')}`);
-    }
+        for (let key of Object.keys(customParams)) {
+            mappedParams.set(key, customParams[key]);
+        }
+        const finalParams = [];
+        for (let [ key, value ] of mappedParams.entries()) {
+            finalParams.push(typeof value === 'string' ? `${key}=${value}` : `${key}=${value.join(',')}`);
+        }
 
-    return {
-        validURL: true,
-        domain: matchedParams[1],
-        querystring: finalParams.join('&')
+        return {
+            validURL: true,
+            domain: matchedParams[1],
+            querystring: finalParams.join('&')
+        }
+    } catch (e) {
+        return {
+            validURL: false
+        }
     }
 }
 
