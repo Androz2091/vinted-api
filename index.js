c38e5aafab5faf3d20631b8d5a4b8ffbec0866f1
ClementG7 <65901807+ClementG7@users.noreply.github.com>
2022-05-12 11:20:22 +0200

    fix error "access denied" (#9)
    Co-authored-by: Androz <androz2091@gmail.com>

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ const fetchCookie = (domain = 'fr') => {
         }
         fetch(`https://vinted.${domain}`, {
             signal: controller.signal,
-            agent
+            agent,
+            headers: {
+                'user-agent': new UserAgent().toString()
+            }
         }).then((res) => {
             const sessionCookie = res.headers.get('set-cookie');
             controller.abort();
@@ -106,7 +109,7 @@ const search = (url, disableOrder = false, allowSwap = false, customParams = {})
         }
 
         const controller = new AbortController();
-        fetch(`https://www.vinted.${domain}/api/v2/catalog/items?${querystring}`, {
+        fetch(`https://www.vinted.be/api/v2/catalog/items?${querystring}`, {
             signal: controller.signal,
             //agent: process.env.VINTED_API_HTTPS_PROXY ? new HttpsProxyAgent(process.env.VINTED_API_HTTPS_PROXY) : undefined,
             headers: {
