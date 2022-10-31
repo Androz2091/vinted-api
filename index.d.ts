d54de9d643d647b3b861222d716c96a66bc2085e
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-10-31 12:02:40 +0100

    Fix typage (#15)

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ declare module 'vinted-api' {
     }
 
     export function fetchCookie (): Promise<string>;
-    export function parseVintedQuerystring (url: string): string;
+    export function parseVintedURL (url: string): {validURL: boolean, domain?: RegExpMatchArray | null, querystring?: string};
     export function search (url: string, disableOrder?: boolean, allowSwap?: boolean, customParams?: Record<string, string|number>): Promise<VintedSearchResult>;
 
 }
